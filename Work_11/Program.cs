886cb8b2d4ff8d6b21c4e3f0940ad60b84be32f1
alexsis-valentinovich <alexsis-valentinovich@gmail.com>
2022-07-22 21:01:56 +0500

    Добавьте файлы проекта.

---
+++ Work_11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//1.   Разработать структуру для решения линейного уравнения 0=kx+b. Коэффициенты уравнения k, b реализовать
+//с помощью полей вещественного типа. Для решения уравнения предусмотреть метод Root. Создать экземпляр
+//разработанной структуры. Осуществить использование экземпляра в программе.
+namespace Part_1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Решение линейного уравнения 0=kX+b \nВведите коэфициент k=");
+            double k=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите коэфициент b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+            linEq LinEq = new linEq(k, b);
+            Console.WriteLine(LinEq.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Work_11/linEq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    struct linEq
+    {
+        double k;
+        double b;
+        public linEq(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0)
+                return "Решений нет";
+            else
+            {
+                double x = -1 * b / k;
+                return $"Решение уравнения = {x:0.000}";
+            }
+        }
+    }
+}
